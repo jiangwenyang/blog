511e6926abb5a3b75d1399b03dcc0df1c641a165
jiangwenyang <u19950930@gmail.com>
2022-03-25 17:31:34 +0800

    feat: 移除注释

--- utils/gitTalk.ts
+++ utils/gitTalk.ts
@@ -1,10 +1,6 @@
 import Gitalk from "gitalk";
 
 const initGitalk = (container: string, id: string) => {
-  console.log("环境变量");
-  console.log(process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_ID);
-  console.log(process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_SECRET);
-
   const gitalkOptions = {
     clientID: process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_ID!,
     clientSecret: process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_SECRET!,
