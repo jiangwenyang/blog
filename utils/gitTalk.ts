59d97aa7daf11f42428214c7330566421f6edb3c
jiangwenyang <u19950930@gmail.com>
2022-03-25 17:23:35 +0800

    feat: 环境变量

--- utils/gitTalk.ts
+++ utils/gitTalk.ts
@@ -2,15 +2,15 @@ import Gitalk from "gitalk";
 
 const initGitalk = (container: string, id: string) => {
   console.log("环境变量");
-  console.log(process.env.GIT_TALK_CLIENT_ID);
-  console.log(process.env.GIT_TALK_CLIENT_SECRET);
+  console.log(process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_ID);
+  console.log(process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_SECRET);
 
   const gitalkOptions = {
-    clientID: process.env.GIT_TALK_CLIENT_ID!,
-    clientSecret: process.env.GIT_TALK_CLIENT_SECRET!,
-    repo: process.env.GIT_TALK_REPO!,
-    owner: process.env.GIT_TALK_OWNER!,
-    admin: [process.env.GIT_TALK_OWNER!],
+    clientID: process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_ID!,
+    clientSecret: process.env.NEXT_PUBLIC_GIT_TALK_CLIENT_SECRET!,
+    repo: process.env.NEXT_PUBLIC_GIT_TALK_REPO!,
+    owner: process.env.NEXT_PUBLIC_GIT_TALK_OWNER!,
+    admin: [process.env.NEXT_PUBLIC_GIT_TALK_OWNER!],
     id,
   };
   const gitalk = new Gitalk(gitalkOptions);
